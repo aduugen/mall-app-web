4b70941866d0c28e596c2ed2ba7e43992fad01e4
aduugen <z_semail@163.com>
2025-04-03 09:12:35 +0800

    登录页面优化

--- pages/public/login.vue
+++ pages/public/login.vue
@@ -123,200 +123,105 @@
 	}
 
 	.container {
-		padding-top: 115px;
 		position: relative;
-		width: 100vw;
+		width: 100%;
 		height: 100vh;
-		overflow: hidden;
-		background: #fff;
 		background: linear-gradient(135deg, #f5f7fa 0%, #e9f0fb 100%);
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		
-		/* 装饰符号元素 */
-		&::before, &::after {
-			content: '';
-			position: absolute;
-			width: 400upx;
-			height: 400upx;
-			z-index: 1;
-			opacity: 0.3;
-		}
-		
-		&::before {
-			background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%234bb0ff"><path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"/></svg>') no-repeat;
-			background-size: contain;
-			top: -50upx;
-			left: -100upx;
-			transform: rotate(-15deg);
-		}
-		
-		&::after {
-			background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%23286090"><path d="M7 5h10v2h2V3c0-1.1-.9-2-2-2H7c-1.1 0-2 .9-2 2v4h2V5zm8 10H7v-2H5v4c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2v-4h-2v2z"/><path d="M20.1 7.7l-1.4-1.4L16 8.9l-2.7-2.7-1.4 1.4 2.7 2.7-2.7 2.7 1.4 1.4 2.7-2.7 2.7 2.7 1.4-1.4-2.7-2.7z"/></svg>') no-repeat;
-			background-size: contain;
-			bottom: 0;
-			right: -100upx;
-			transform: rotate(15deg);
-		}
-	}
-	
-	.wrapper {
-		position: relative;
-		background: rgba(255, 255, 255, 0.02);
-		z-index: 90;
-		//padding-bottom: 40upx;
-		border-radius: 25upx;
-		margin: 0 30upx;
-		box-shadow: 0 15upx 40upx rgba(0, 0, 0, 0.04);
-		backdrop-filter: blur(10px);
-		border: 1px solid rgba(255, 255, 255, 0.8);
-		width: auto;
-		max-width: 750upx;
-		min-height: 50vh;
-		max-height: 80vh;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
+		padding: 40upx;
+		box-sizing: border-box;
 	}
 
 	.back-btn {
 		position: absolute;
 		left: 40upx;
-		z-index: 9999;
-		padding-top: var(--status-bar-height);
 		top: 40upx;
+		padding-top: var(--status-bar-height);
 		font-size: 40upx;
 		color: $font-color-dark;
+		z-index: 999;
+	}
+
+	.wrapper {
+		position: relative;
+		width: 100%;
+		padding: 50upx 40upx;
+		margin-top: 150upx;
+		background: rgba(255, 255, 255, 0.95);
+		border-radius: 20upx;
+		box-shadow: 0 15upx 40upx rgba(0, 0, 0, 0.1);
+		box-sizing: border-box;
 	}
 
 	.left-top-sign {
-		font-size: 70upx;
-		color: $page-color-base;
-		position: relative;
-		left: 50upx;
-		opacity: 0.8;
-		background: linear-gradient(to right, #f1f2f3, #5b4373);
+		font-size: 56upx;
+		font-weight: 500;
+		margin-bottom: 60upx;
+		background: linear-gradient(to right, #4bb0ff, #6149f6);
 		-webkit-background-clip: text;
 		color: transparent;
-		text-shadow: 0 5upx 15upx rgba(40, 96, 144, 0.2);
-	}
-	
-	.welcome {
-		position: relative;
-		left: 50upx;
-		top: -90upx;
-		font-size: 46upx;
-		color: #286090;
-		text-shadow: 1px 0px 1px rgba(0, 0, 0, .2);
-		font-weight: bold;
-		letter-spacing: 2upx;
 	}
 
 	.input-content {
-		padding: 0 60upx;
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
+		padding: 0;
 	}
 
 	.input-item {
 		display: flex;
 		flex-direction: column;
-		align-items: flex-start;
-		justify-content: center;
 		padding: 0 30upx;
-		background: rgba(255, 255, 255, 0.8);
+		background: #f8f8f8;
 		height: 120upx;
-		border-radius: 10upx;
-		margin-bottom: 50upx;
-		box-shadow: 0 3upx 10upx rgba(0, 0, 0, 0.05);
+		border-radius: 4px;
+		margin-bottom: 30upx;
 		border-left: 4upx solid #4bb0ff;
-		transition: all 0.3s;
+	}
 
-		&:focus-within {
-			border-left: 4upx solid #286090;
-			background: rgba(255, 255, 255, 0.95);
-			box-shadow: 0 3upx 15upx rgba(0, 0, 0, 0.1);
-		}
+	.input-item:last-child {
+		margin-bottom: 40upx;
+	}
 
-		&:last-child {
-			margin-bottom: 0;
-		}
+	.input-item .tit {
+		height: 40upx;
+		line-height: 40upx;
+		font-size: 28upx;
+		color: #606266;
+	}
 
-		.tit {
-			height: 50upx;
-			line-height: 56upx;
-			font-size: $font-sm+2upx;
-			color: #286090;
-			font-weight: bold;
-		}
-
-		input {
-			height: 60upx;
-			font-size: $font-base + 2upx;
-			color: $font-color-dark;
-			width: 100%;
-		}
+	.input-item input {
+		height: 60upx;
+		font-size: 30upx;
+		color: #303133;
+		width: 100%;
 	}
 
 	.confirm-btn {
-		position: relative;
-		width: 430upx;
-		height: 76upx;
-		line-height: 76upx;
-		border-radius: 50px;
-		margin-top: 70upx;
-		background: linear-gradient(to right, #4bb0ff, #286090);
+		width: 100%;
+		height: 90upx;
+		line-height: 90upx;
+		border-radius: 6upx;
+		font-size: 32upx;
 		color: #fff;
-		font-size: $font-lg;
-		box-shadow: 0 5upx 20upx rgba(40, 96, 144, 0.3);
-		transition: all 0.3s;
-		
-		&:active {
-			transform: scale(0.98);
-			box-shadow: 0 3upx 10upx rgba(40, 96, 144, 0.2);
-		}
-
-		&:after {
-			border-radius: 100px;
-		}
-	}
-	
-	.forget-section {
-	    position: relative;
-		font-size: $font-sm+2upx;
-		color: #286090;
-		text-align: center;
-		margin-top: 40upx;
-		font-weight: bold;
-		
-		&:active {
-			opacity: 0.7;
-		}
+		margin-top: 50upx;
+		background: linear-gradient(to right, #4bb0ff, #286090);
 	}
 
 	.register-section {
-		position: relative;
-		left: 0;
-		width: 100%;
-		font-size: $font-sm+2upx;
-		color: $font-color-base;
-		text-align: center;
-		z-index: 95;
-		background: rgba(255, 255, 255, 0.02);
+		margin-top: 40upx;
 		padding: 20upx 0;
-		backdrop-filter: blur(10px);
+		text-align: center;
+		font-size: 28upx;
+		color: #606266;
+	}
 
-		text {
-			color: #286090;
-			margin-left: 10upx;
-			font-weight: bold;
-			
-			&:active {
-				opacity: 0.7;
-			}
-		}
+	.register-section text {
+		color: #4bb0ff;
+		margin-left: 10upx;
+	}
+
+	.forget-section {
+		text-align: center;
+		font-size: 28upx;
+		color: #606266;
+		margin-top: 20upx;
 	}
 </style>
